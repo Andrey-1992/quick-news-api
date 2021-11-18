d431971c61862379623c2407cf1b4894b08dfa2b
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-11-17 17:43:33 -0700

    set up controller get method for controller file

--- dist/controllers/index.controller.js
+++ dist/controllers/index.controller.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSavedNews = void 0;
+const getSavedNews = (req, res) => {
+    res.send('test get news');
+};
+exports.getSavedNews = getSavedNews;
---
+++ src/controllers/index.controller.ts
@@ -0,0 +1,5 @@
+import {Request, Response} from 'express'
+
+export const getSavedNews = (req: Request, res: Response) => {
+  res.send('test get news')
+}
