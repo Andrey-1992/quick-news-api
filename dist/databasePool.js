ec1cf262daa05ad46ed1d10cbfad703b970696ab
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-08 13:05:39 -0600

    Config pool obj connector to Node

--- dist/databasePool.js
+++ dist/databasePool.js
@@ -1,2 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.pool = void 0;
+const pg_1 = require("pg");
+exports.pool = new pg_1.Pool({
+    user: 'postgres',
+    host: 'localhost',
+    password: '',
+    port: 5432
+});
--- src/databasePool.ts
+++ src/databasePool.ts
@@ -1,1 +1,8 @@
 import { Pool} from 'pg';
+
+export const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  password: '',
+  port: 5432
+})
