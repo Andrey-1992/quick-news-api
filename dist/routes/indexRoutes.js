6d71e09ab2b995c7d5c4c0c78aaa4f535b6e90c0
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-08 13:15:14 -0600

    Update controller file naming convention

--- dist/routes/indexRoutes.js
+++ dist/routes/indexRoutes.js
@@ -3,4 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 router.get('/saved-news', (req, res) => res.send('testing route'));
+// router.post('/saved-news', (req, res) => res.send('testing route'));
+// router.delete('/saved-news', (req, res) => res.send('testing route'));
 exports.default = router;
--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,8 +1,10 @@
 
 import {Router} from 'express';
 const router = Router();
-import {getSavedNews} from '../controllers/index.controller'
+import {getSavedNews} from '../controllers/indexController'
 
 router.get('/saved-news', (req, res) => res.send('testing route'));
+// router.post('/saved-news', (req, res) => res.send('testing route'));
+// router.delete('/saved-news', (req, res) => res.send('testing route'));
 
 export default router;
