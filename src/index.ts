967eca87fc5b602ccb46a51594a49de64e5ce271
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-15 15:06:55 -0600

    Add port number to the project

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 const app = express(); 
-// const cors = require('cors');
 import cors from 'cors';
 
-import indexRoutes from './routes/indexRoutes'
+import indexRoutes from './routes/indexRoutes';
+
+app.locals.title = 'Strange prompts'
+app.set('port', process.env.PORT || 3001);
 
 app.use(express.json());
-app.use(indexRoutes)
+app.use(indexRoutes);
+
+app.get('/', (req, res) => {
+  res.status(200).send(`You've reached the ${app.locals.title} API `)
+});
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000")
-})
+  console.log(`Server is listening on ${}`)
+});
