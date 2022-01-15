37603bccd5890d6aa993f332f9a597e01b438a78
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-15 15:15:05 -0600

    Config the pool object to heroku config

--- dist/databasePool.js
+++ dist/databasePool.js
@@ -6,12 +6,18 @@ require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 exports.pool = new pg_1.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    port: 5432,
-    database: process.env.DB_DATABASE,
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
     ssl: {
         rejectUnauthorized: false,
     }
 });
+/*
+ user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+  database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+*/ 
--- src/databasePool.ts
+++ src/databasePool.ts
@@ -5,7 +5,14 @@ const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: { 
+    rejectUnauthorized: false,
+  }
+})
+
+/*
+ user: process.env.DB_USER,
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   port: 5432,
@@ -13,4 +20,4 @@ export const pool = new Pool({
   ssl: {
     rejectUnauthorized: false,
   }
-})
+*/
