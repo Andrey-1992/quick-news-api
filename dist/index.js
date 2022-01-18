652981fc1a865ac8ca4e6250bca09e68e6ee40d8
Andrey Bermudez <andrey.bermudez92@gmail.com>
2022-01-18 09:11:48 -0600

    Set up cors port for the project

--- dist/index.js
+++ dist/index.js
@@ -1,47 +1,15 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors = __importStar(require("cors"));
+// import * as cors from 'cors';
 const app = (0, express_1.default)();
+var cors = require('cors');
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 app.locals.title = 'Quick News Server';
-// app.set('port', process.env.DB_PORT || 3000);
 const port = process.env.PORT || 4000;
-const options = {
-    allowedHeaders: [
-        'Origin',
-        'X-Requested-With',
-        'Content-Type',
-        'Accept',
-        'X-Access-Token',
-    ],
-    credentials: true,
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: API_URL,
-    preflightContinue: false,
-};
 app.use(express_1.default.json());
 app.use(cors());
 app.use(indexRoutes_1.default);
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,16 @@
 import express from 'express';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 const app = express(); 
+
+var cors = require('cors');
 
 import indexRoutes from './routes/indexRoutes';
 
 app.locals.title = 'Quick News Server'
-// app.set('port', process.env.DB_PORT || 3000);
 const port: any = process.env.PORT || 4000;
 
-const options: cors.CorsOptions = {
-  allowedHeaders: [
-    'Origin',
-    'X-Requested-With',
-    'Content-Type',
-    'Accept',
-    'X-Access-Token',
-  ],
-  credentials: true,
-  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin: API_URL,
-  preflightContinue: false,
-};
-
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(indexRoutes);
 
 app.get('/', (req, res) => {
